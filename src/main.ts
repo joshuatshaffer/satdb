dc135f280f04529dfb78f91cc105a90a9b25996b
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-11-19 21:17:21 -0600

    Add database connection.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import Fastify from "fastify";
+import { db } from "./db/db";
 
 const tleSource =
   "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle";
 
 const fastify = Fastify({
   logger: true,
+});
+
+fastify.get("/health/db", async (request, reply) => {
+  const result = await db.run("select 1");
+  return { status: "ok", result };
 });
 
 fastify.get("/tle", async (request, reply) => {
