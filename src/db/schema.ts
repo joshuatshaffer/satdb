53e45408a7d56fb25d2cdf0db1572d4f333a182a
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-11-19 22:33:43 -0600

    Download JSON data from CelesTrak.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,27 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const tles = sqliteTable("tle", {
   name: text(),
   line1: text(),
   line2: text(),
+});
+
+export const Satellites = sqliteTable("satellites", {
+  objectName: text(),
+  objectId: text(),
+  epoch: text(),
+  meanMotion: real(),
+  eccentricity: real(),
+  inclination: real(),
+  raOfAscNode: real(),
+  argOfPericenter: real(),
+  meanAnomaly: real(),
+  ephemerisType: integer(),
+  classificationType: text(),
+  noradCatId: integer().notNull().unique(),
+  elementSetNo: integer(),
+  revAtEpoch: integer(),
+  bstar: real(),
+  meanMotionDot: real(),
+  meanMotionDDot: real(),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { InferInsertModel } from "drizzle-orm";
 import Fastify from "fastify";
+import { CelesTrakJson } from "./CelesTrakJson";
 import { db } from "./db/db";
 import * as schema from "./db/schema";
 
 const tleSource =
   "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle";
+const satelliteJsonSource =
+  "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=json";
 
 const fastify = Fastify({
   logger: true,
@@ -34,6 +38,56 @@ fastify.get("/tle/refresh", async (request, reply) => {
 
   await db.delete(schema.tles);
   await db.insert(schema.tles).values(tles);
+});
+
+function celesTrakJsonToSatellite(
+  sat: CelesTrakJson
+): InferInsertModel<typeof schema.Satellites> {
+  return {
+    objectName: sat.OBJECT_NAME,
+    objectId: sat.OBJECT_ID,
+    epoch: sat.EPOCH,
+    meanMotion: sat.MEAN_MOTION,
+    eccentricity: sat.ECCENTRICITY,
+    inclination: sat.INCLINATION,
+    raOfAscNode: sat.RA_OF_ASC_NODE,
+    argOfPericenter: sat.ARG_OF_PERICENTER,
+    meanAnomaly: sat.MEAN_ANOMALY,
+    ephemerisType: sat.EPHEMERIS_TYPE,
+    classificationType: sat.CLASSIFICATION_TYPE,
+    noradCatId: sat.NORAD_CAT_ID,
+    elementSetNo: sat.ELEMENT_SET_NO,
+    revAtEpoch: sat.REV_AT_EPOCH,
+    bstar: sat.BSTAR,
+    meanMotionDot: sat.MEAN_MOTION_DOT,
+    meanMotionDDot: sat.MEAN_MOTION_DDOT,
+  };
+}
+
+function* batch<T>(arr: readonly T[], batchSize: number) {
+  for (let i = 0; i < arr.length; i += batchSize) {
+    yield arr.slice(i, i + batchSize);
+  }
+}
+
+fastify.get("/satellites/refresh", async (request, reply) => {
+  const response = await fetch(satelliteJsonSource);
+  const json: CelesTrakJson[] = await response.json();
+  const satellites = json.map(celesTrakJsonToSatellite);
+
+  fastify.log.info(`Downloaded and parsed ${satellites.length} satellites`);
+
+  await db.delete(schema.Satellites);
+  for (const satelliteBatch of batch(satellites, 400)) {
+    fastify.log.info(`Inserting ${satelliteBatch.length} satellites`);
+    await db.insert(schema.Satellites).values(satelliteBatch);
+  }
+
+  return { numberSatellites: satellites.length };
+});
+
+fastify.get("/satellites", async (request, reply) => {
+  return await db.select().from(schema.Satellites).limit(10);
 });
 
 async function start() {
